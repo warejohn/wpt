f1c54968870855663254b781047a2bd8b07bb95f
Majid Valipour <majidvp@chromium.org>
2019-10-31 10:53:41 -0700

    [scroll-snap] Move keyboard test to WPT
    Move basic keyboard snapping to WPT.
    
    Changes:
     - Replace Blink event sender usage with test_driver usage
     - Add common style and script that I expect to be used in upcoming tests
     - Remove unnecessary logic to click on scroller
     - Move clean up logic with add_cleanup to ensure they run even if a test
       fail
    
    BUG: 1011066
    Change-Id: I83db1708277d2d469c940a32ea549edec8c62d23

---
+++ css/css-scroll-snap/support/common.js
@@ -0,0 +1,56 @@
+const KEY_CODE_MAP = {
+  'ArrowLeft':  '\uE012',
+  'ArrowUp':    '\uE013',
+  'ArrowRight': '\uE014',
+  'ArrowDown':  '\uE015',
+  'PageUp':     '\uE00E',
+  'PageDown':   '\uE00F',
+  'End':        '\uE010',
+  'Home':       '\uE011',
+  'Space':      ' ',
+};
+
+// Send key event to the target element using test driver. Supports human
+// friendly key names for common keyboard scroll operations e.g., arrow keys,
+// page key, etc.
+async function keyPress(target, key) {
+  let code = key;
+  if (KEY_CODE_MAP.hasOwnProperty(key))
+    code = KEY_CODE_MAP[key];
+
+  return test_driver.send_keys(target, code);
+}
+
+// Use rAF to wait for the value of the getter function passed to not change for
+// at least 15 frames or timeout after 1 second.
+//
+// Example usage:
+//    await waitForAnimationEnd(() => scroller.scrollTop);
+function waitForAnimationEnd(getValue) {
+  const TIMEOUT = 1000; // milliseconds
+  const MAX_UNCHANGED_FRAMES = 15;
+
+  const start_time = performance.now();
+  let last_changed_frame = 0;
+  let last_value = getValue();
+
+  return new Promise((resolve, reject) => {
+    function tick(frames, time) {
+    // We requestAnimationFrame either for TIMEOUT milliseconds or until
+    // MAX_UNCHANGED_FRAMES with no change have been observed.
+      if (time - start_time > TIMEOUT
+        || frames - last_changed_frame >= MAX_UNCHANGED_FRAMES) {
+        resolve();
+      } else {
+        current_value = getValue();
+        if (last_value != current_value) {
+          last_changed_frame = frames;
+          last_value = current_value;
+        }
+        requestAnimationFrame(tick.bind(this, frames + 1));
+      }
+    }
+    tick(0, start_time);
+  });
+}
+
